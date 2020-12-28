8dde6432e1d26a8be18eb78b25c6ffa656baf42d
MamadTvl <mtavallai80@gmail.com>
2020-12-28 05:32:34 +0330

    add toEnDigit

--- src/utills/ToFaDigit.js
+++ src/utills/ToFaDigit.js
@@ -1,14 +1,26 @@
-
-
 const isNumber = (str) => {
     return /^\d+$/.test(str)
 }
 
+const isPersianNumber = (charCode) => {
+    return (charCode >= 1776 && charCode <= 1785)
+}
+export const toEnDigit = (text) => {
+    let ret = '';
+    for (let i = 0; i < text.length; i++) {
+        if (isPersianNumber(text.charCodeAt(i)))
+            ret += String.fromCharCode(text.charCodeAt(i) - 1728);
+        else
+            ret += text[i]
+    }
+
+    return ret;
+}
 
 export const toFaDigit = (text) => {
     let ret = '';
     for (let i = 0; i < text.length; i++) {
-        if(isNumber(text[i]))
+        if (isNumber(text[i]))
             ret += String.fromCharCode(text.charCodeAt(i) + 1728);
         else
             ret += text[i]
@@ -16,6 +28,7 @@ export const toFaDigit = (text) => {
 
     return ret;
 }
+
 function reverseString(str) {
     return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
 }
@@ -24,12 +37,12 @@ export const separateDigit = (number) => {
     let count = 0
     number = toFaDigit(number.toString())
     let separatedNum = ''
-    for(let i= number.length - 1 ; i >= 0 ; i--){
-        if (count < 3){
-            separatedNum+= number[i]
+    for (let i = number.length - 1; i >= 0; i--) {
+        if (count < 3) {
+            separatedNum += number[i]
             count++
         }
-        if (count === 3){
+        if (count === 3) {
             separatedNum += '٫'
             count = 0
         }
